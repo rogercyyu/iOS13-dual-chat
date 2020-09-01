55f388506c68d65b500312ef3e9cfb1cb9e2ca07
Roger Yu <roger@Rogers-MacBook-Pro.local>
2020-09-01 15:57:58 -0400

    initial commit

---
+++ Dual Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alert = UIAlertController(title: e.localizedDescription, message: "", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    // Navigate to the chat view controller
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+            }
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Dual-Chat
+
+## About
+
+- Simple chatting app using Google Firebase authentication and MVC design patterns for iOS 13.
+- Created by following online tutorials and implementing additional features.
+
